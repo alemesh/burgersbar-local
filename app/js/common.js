69f09b920625ccf725add63910c8c90439bac900
alemesh <alemesh@acceptic.com>
2018-08-10 18:33:37 +0300

    added desctop header

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,21 @@
 $(function() {
 
 	// Custom JS
+    (function($) {
+        $('.variable-width').slick({
+            dots: false,
+            infinite: true,
+            // rtl: true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            // centerMode: true,
+            prevArrow: '<span class="slick-prev"></span>',
+            nextArrow: '<span class="slick-next"></span>',
+            // variableWidth: true,
+            autoplay: true,
+            autoplaySpeed: 3000
+        });
+    })(jQuery);
 
 });
